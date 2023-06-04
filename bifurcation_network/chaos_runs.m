9f7cc72497727c51b7aa04fe89aa7632a2b1b2b9
Ian Wilson <i@nwilson.dev>
2023-06-04 08:52:17 -0700

    clarify directory

--- bifurcation_network/chaos_runs.m
+++ bifurcation_network/chaos_runs.m
@@ -16,17 +16,18 @@ for file=1:10
     clear workspace;
     run network_geometry;
     run network_params;
-    
+
     load chaotic_base % set parameters for chaotic delta
     timesteps=400000;
     slope_perturb_amp = 0;
     init_perturb_amp = 0;
-    
+
     run network_initialize;
     run network_mainfile
-    
+
     filename = strcat('eta_perturbed/', int2str(file));
     filename
     save(filename);
 end
 
+save eta_perturbed
